6ac58c9c4be85508c01b732c5c9f749b2e3098f0
shukonin <fernando.canas@outlook.com>
2020-12-16 19:07:23 -0500

    add/ Payment History

--- src/Dashboard/Components/PaymentHistory/index.jsx
+++ src/Dashboard/Components/PaymentHistory/index.jsx
@@ -1,7 +1,51 @@
 import React from "react";
 
+import "../../Styles/fonts.css";
+import {
+  Title,
+  TitleContainer,
+  PaymentStatContainer,
+  PaymentStatGraph,
+  PaymentStatContent,
+  PaymentStatTitle,
+  PaymentStatValue,
+  Settings,
+} from "./style";
+
+const PaymentStat = (props) => {
+  const { graph, title, value, color } = props;
+  return (
+    <PaymentStatContainer color={color}>
+      <PaymentStatGraph>{graph}</PaymentStatGraph>
+      <PaymentStatContent>
+        <PaymentStatTitle>{title}</PaymentStatTitle>
+        <PaymentStatValue>${value}</PaymentStatValue>
+      </PaymentStatContent>
+    </PaymentStatContainer>
+  );
+};
+
 const PaymentHistory = () => {
-  return <h1>P</h1>;
+  return (
+    <>
+      <TitleContainer>
+        <Title>Payment History</Title>
+        <Settings className="icon-equalizer" />
+      </TitleContainer>
+      <PaymentStat
+        graph="75%"
+        title="Average Income"
+        value="12,380.00"
+        color="black"
+      />
+      <PaymentStat
+        graph="25%"
+        title="Average Spend"
+        value="2,620.00"
+        color="blue"
+      />
+    </>
+  );
 };
 
 export { PaymentHistory };
--- src/Dashboard/Components/PaymentHistory/style.js
+++ src/Dashboard/Components/PaymentHistory/style.js
@@ -1,1 +1,66 @@
+import styled from "styled-components";
 
+const TitleContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 1rem 0 0;
+`;
+
+const Title = styled.h3`
+  margin: 0;
+`;
+
+const Settings = styled.i`
+  &:hover {
+    cursor: pointer;
+    color: orange;
+  }
+`;
+
+const PaymentStatContainer = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: ${(props) => props.color};
+  border-radius: 1rem;
+  margin: 1rem 0;
+
+  &:hover {
+    cursor: pointer;
+    box-shadow: 2px 2px 10px 1px gray;
+  }
+`;
+
+const PaymentStatGraph = styled.p`
+  color: white;
+  font-weight: bold;
+  margin: 0 1rem;
+`;
+
+const PaymentStatContent = styled.div`
+  margin: 0.8rem;
+`;
+
+const PaymentStatTitle = styled.p`
+  margin: 0;
+  color: white;
+  font-size: 0.8rem;
+`;
+
+const PaymentStatValue = styled.p`
+  margin: 0;
+  font-size: 1.3rem;
+  font-weight: bold;
+  color: white;
+`;
+
+export {
+  Title,
+  Settings,
+  TitleContainer,
+  PaymentStatContainer,
+  PaymentStatGraph,
+  PaymentStatContent,
+  PaymentStatTitle,
+  PaymentStatValue,
+};
