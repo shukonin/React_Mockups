6459d719da1b4430e417946134c78c144990dff7
shukonin <fernando.canas@outlook.com>
2021-02-18 15:56:03 -0500

    Add/ Single Page Mockup basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { Dashboard } from "./Dashboard/Dashboard";
-import { Home } from "./Home";
-import { Signup1 } from "./Signup_1/Signup1";
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Dashboard } from './Dashboard/Dashboard';
+import { Home } from './Home';
+import { Signup1 } from './Signup_1/Signup1';
+import { SinglePage } from './Single_Page/SinglePage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route exact path="/React_Mockups/" component={Home} />
         <Route exact path="/React_Mockups/signup1" component={Signup1} />
         <Route exact path="/React_Mockups/dashboard" component={Dashboard} />
+        <Route exact path="/React_Mockups/singlepage" component={SinglePage} />
       </Switch>
     </BrowserRouter>
   );
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
@@ -11,6 +11,9 @@ const Home = () => {
         </li>
         <li>
           <Link to="/React_Mockups/dashboard">Dashboard</Link>
+        </li>
+        <li>
+          <Link to="/React_Mockups/SinglePage">Single Page</Link>
         </li>
       </ul>
     </>
